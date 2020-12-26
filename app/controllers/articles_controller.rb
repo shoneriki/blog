8cd541aa3ebdde83de1b44285188cf3e0f75ebdc
shoneriki <ikirenohs@gmail.com>
2020-12-27 01:09:20 +0900

    form partial

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -21,7 +21,27 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit
+    end
+  end
+
+
+
   private
+
+  # def set_article
+  #   @article = Article.find(params[:id])
+  # end
 
   def article_params
     params.require(:article).permit(:title, :body)
